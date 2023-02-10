ce40bc18fb3be00a9189f11a0436604957e660c7
Sonu Pant007 <sonupant007@gmail.com>
2023-02-10 20:18:49 +0530

    update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const fetchResponse = async (chat) => {
   try {
     // after depoloyment you should change the fetch URL below
-    const response = await fetch("https://serve-chat-gpt2-0.vercel.app/", {
+    const response = await fetch("http://localhost:3080", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
